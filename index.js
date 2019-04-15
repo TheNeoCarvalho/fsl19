54640bc507c1fc43d4c2e09d42db2055a8bc5ecb
Manoel Carvalho <manoelmecat@gmail.com>
2019-04-15 12:29:14 -0300

    Bootstrap

--- index.js
+++ index.js
@@ -16,22 +16,14 @@ app.use(
   })
 );
 
-const init = async () => {
-  const db = await dbConnection;
-  await db.run(
-    `CREATE TABLE IF NOT EXISTS categoria (id INTEGER PRIMARY KEY, categoria TEXT);`
-  );
-  await db.run(
-    `CREATE TABLE IF NOT EXISTS vaga (id INTEGER PRIMARY KEY, titulo TEXT, descricao TEXT, categoria INTEGER);`
-  );
-};
-
-init();
-
 app.get("/", async (req, res) => {
   const db = await dbConnection;
   const categoriasDb = await db.all("SELECT * FROM categoria");
   const vagas = await db.all("SELECT * FROM vaga");
+
+  const alunos = await db.all("SELECT * FROM alunos");
+  const empresas = await db.all("SELECT * FROM empresas");
+
   const categorias = categoriasDb.map(cat => {
     return {
       ...cat,
@@ -39,7 +31,9 @@ app.get("/", async (req, res) => {
     };
   });
   res.render("home", {
-    categorias
+    categorias,
+    alunos,
+    empresas
   });
 });
 
@@ -99,95 +93,93 @@ app.get("/admin/vagas/nova", async (req, res) => {
   const categorias = await db.all("SELECT * FROM categoria");
   res.render("adm/cadastroVagas", {
     categorias
-  })
-})
+  });
+});
 
 app.post("/admin/vagas/nova", async (req, res) => {
-  let {
-    titulo,
-    categoria,
-    descricao
-  } = req.body;
+  let { titulo, categoria, descricao } = req.body;
   const db = await dbConnection;
   await db.run(
     `INSERT INTO vaga (titulo, descricao, categoria) VALUES ('${titulo}', '${descricao}', ${categoria});`
   );
   res.redirect("/admin/vagas");
-})
+});
 
 app.get("/admin/categorias/nova", (req, res) => {
   res.render("adm/cadastroCategoria");
-})
+});
 
 app.post("/admin/categorias/nova", async (req, res) => {
-  let {
-    categoria
-  } = req.body;
+  let { categoria } = req.body;
   const db = await dbConnection;
-  await db.run(
-    `INSERT INTO categoria (categoria) VALUES ('${categoria}');`
-  );
+  await db.run(`INSERT INTO categoria (categoria) VALUES ('${categoria}');`);
   res.redirect("/admin/categorias");
-})
+});
 
 app.get("/admin/vagas/update/:id", async (req, res) => {
-  let {
-    id
-  } = req.params;
+  let { id } = req.params;
   const db = await dbConnection;
-  const vaga = await db.get(
-    `SELECT * FROM vaga WHERE id = ${id};`
-  );
+  const vaga = await db.get(`SELECT * FROM vaga WHERE id = ${id};`);
   let cat = vaga.categoria;
-  const categoria = await db.get(
-    `SELECT * FROM categoria WHERE id = ${cat};`
-  );
+  const categoria = await db.get(`SELECT * FROM categoria WHERE id = ${cat};`);
   res.render("adm/atualizaVagas", {
     vaga,
     categoria
   });
-})
+});
 
 app.post("/admin/vagas/update/:id", async (req, res) => {
-  let {
-    id,
-    titulo,
-    categoria,
-    descricao
-  } = req.body;
+  let { id, titulo, categoria, descricao } = req.body;
   let idUrl = req.params.id;
   const db = await dbConnection;
   await db.run(
-    `UPDATE vaga SET titulo= '${titulo}', descricao = '${descricao}', categoria = '${categoria}' WHERE id = ${idUrl};`
+    `UPDATE vaga SET titulo= '${titulo}', descricao = '${descricao}' WHERE id = ${id};`
   );
   res.redirect("/admin/vagas");
-})
+});
 
 app.get("/admin/categorias/update/:id", async (req, res) => {
-  let {
-    id
-  } = req.params;
+  let { id } = req.params;
   const db = await dbConnection;
-  const categoria = await db.get(
-    `SELECT * FROM categoria WHERE id = ${id};`
-  );
+  const categoria = await db.get(`SELECT * FROM categoria WHERE id = ${id};`);
   res.render("adm/atualizaCategorias", {
     categoria
   });
-})
+});
 
 app.post("/admin/categorias/update/:id", async (req, res) => {
-  let {
-    id,
-    categoria
-  } = req.body;
+  let { id, categoria } = req.body;
   let idUrl = req.params.id;
   const db = await dbConnection;
   await db.run(
-    `UPDATE categoria SET categoria = '${categoria}' WHERE id = ${idUrl};`
+    `UPDATE categoria SET categoria = '${categoria}' WHERE id = ${id};`
   );
-  res.redirect("/admin/categorias")
-})
+  res.redirect("/admin/categorias");
+});
+
+const init = async () => {
+  const db = await dbConnection;
+  await db.run(
+    `CREATE TABLE IF NOT EXISTS categoria (id INTEGER PRIMARY KEY, categoria TEXT);`
+  );
+  await db.run(
+    `CREATE TABLE IF NOT EXISTS vaga (id INTEGER PRIMARY KEY, titulo TEXT, descricao TEXT, categoria INTEGER);`
+  );
+  await db.run(
+    `CREATE TABLE IF NOT EXISTS alunos (id INTEGER PRIMARY KEY, nome TEXT, curso TEXT);`
+  );
+  await db.run(
+    `CREATE TABLE IF NOT EXISTS empresas (id INTEGER PRIMARY KEY, nome TEXT);`
+  );
+
+  // await db.run(
+  //   `INSERT INTO alunos (nome, curso) VALUES ('Henri Enrique Pinheiro', 'Informática');`
+  // );
+
+  // await db.run(`INSERT INTO empresas (nome) VALUES ('W&C Digital');`);
+};
+
+//init();
 
 app.listen(3000, () => {
   console.log("Server is On");
